034218bf40c125dbe5fff51a1affa51d0678734f
Zshxy1013 <838511659@qq.com>
2022-12-07 09:35:05 +0800

    beta 1.0

--- src/components/TeacherDashBoard.vue
+++ src/components/TeacherDashBoard.vue
@@ -53,7 +53,23 @@ export default {
             backgroundStyle: {
               color: 'rgba(220, 220, 220, 0.8)'
             }
-          }]
+          }],
+          tooltip: { // 鼠标悬浮提示框显示 X和Y 轴数据
+            trigger: 'axis',
+            backgroundColor: 'rgba(32, 33, 36,.7)',
+            borderColor: 'rgba(32, 33, 36,0.20)',
+            borderWidth: 1,
+            textStyle: { // 文字提示样式
+              color: '#fff',
+              fontSize: '12'
+            },
+            // axisPointer: { // 坐标轴虚线
+            //   type: 'cross',
+            //   label: {
+            //     backgroundColor: '#6a7985'
+            //   }
+            // }
+          }
         };
         this.myChart.setOption(option);
       })
